03248a26837c75f88a7245e5e65cde8f16035dcb
kayrahman <k.rahman0511@gmail.com>
2020-09-18 10:49:14 +0600

    introduced Compact functions and filters

--- src/AMS.kt
+++ src/AMS.kt
@@ -2,6 +2,7 @@ import java.util.*
 
 fun main(args:Array<String>){
     feedThefish()
+
 }
 
 fun feedThefish() {
@@ -13,11 +14,48 @@ fun feedThefish() {
         println("Change the water today")
     }
 
+
+    //Filters example
+
+    eagerExample()
+
 }
 
-fun shouldChangeWater(day: String,temperature:Int = 22, dirty:Int = 20) : Boolean {
-    return true
+//filter
+fun eagerExample() {
+    val decorations = listOf("Rock","Flowerpot","Pagoda","Plastic plant","Alligator")
+    val eager = decorations.filter { it[0] == 'P'}
+    println(eager)
+
+    val filtered = decorations.asSequence().filter { it[0] == 'P' }
+    println(filtered.toList())
+
+    val lazyMap = decorations.asSequence().map {
+        println("map:$it")
+        it
+    }
+
+    println(lazyMap)
+    println("first: ${lazyMap.first()}")
+    println("all: ${lazyMap.toList()}")
+
 }
+
+fun shouldChangeWater(day: String,temperature:Int = 22, dirty:Int = getDirtySensor()) : Boolean {
+    return when {
+        isTooHot(temperature) -> true
+        isDirty(dirty) -> true
+        isSunday(day) -> true
+        else -> false
+    }
+}
+fun getDirtySensor() = 20
+
+//compact functions
+fun isTooHot(temperature: Int) = temperature >30
+fun isDirty(dirty: Int) = dirty>20
+fun isSunday(day: String) = day=="Sunday"
+
 
 fun fishFood(randomDay: String): Any {
     return when(randomDay){
